9e4a1d71e8210efe6eb5aec59a8198edf659b93e
ziwenxie <ziwenxiecat@gmail.com>
2017-08-23 14:03:10 +0800

    first commit

---
+++ nio/WebClient.java
@@ -0,0 +1,28 @@
+package nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
+/**
+ * @author https://github.com/ziwenxie
+ */
+public class WebClient {
+    public static void main(String[] args) {
+        try {
+            SocketChannel socketChannel = null;
+            socketChannel = SocketChannel.open();
+            socketChannel.connect(new InetSocketAddress("127.0.0.1", 5000));
+
+            ByteBuffer writeBuffer = ByteBuffer.allocate(128);
+            writeBuffer.put("hello world".getBytes());
+
+            writeBuffer.flip();
+            socketChannel.write(writeBuffer);
+            socketChannel.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ nio/WebServer.java
@@ -0,0 +1,34 @@
+package nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+
+/**
+ * @author https://github.com/ziwenxie
+ */
+public class WebServer {
+    public static void main(String args[]) {
+        try {
+            ServerSocketChannel ssc = ServerSocketChannel.open();
+            ssc.socket().bind(new InetSocketAddress("127.0.0.1", 5000));
+            SocketChannel socketChannel = ssc.accept();
+
+            ByteBuffer readBuffer = ByteBuffer.allocate(128);
+            socketChannel.read(readBuffer);
+
+            readBuffer.flip();
+            while (readBuffer.hasRemaining()) {
+                System.out.println((char)readBuffer.get());
+            }
+
+            socketChannel.close();
+            ssc.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
